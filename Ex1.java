fa9c660e95f29089b69a8fd36e1122c0d52e71fd
Gayatri-k3 <gayatrik1029@gmail.com>
2025-03-11 07:59:09 +0530

    Update Ex1.java

---
+++ Ex1.java
@@ -0,0 +1,114 @@
+public class Ex1 {
+    // 1
+    public static void bali(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 2
+    public static void bali(double price) {
+        System.out.println("Price: $" + price);
+    }
+    
+    // 3
+    public static void Paris(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 4
+    public static void Paris(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 5
+    public static void Tokyo(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 6
+    public static void Tokyo(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 7
+    public static void NewYork(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 8
+    public static void NewYork(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 9
+    public static void Dubai(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 10
+    public static void Dubai(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 11
+    public static void London(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 12
+    public static void London(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 13
+    public static void Sydney(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 14
+    public static void Sydney(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 15
+    public static void Rome(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 16
+    public static void Rome(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 17
+    public static void Bangkok(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 18
+    public static void Bangkok(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    // 19
+    public static void Singapore(String name) {
+        System.out.println("Destination: " + name);
+    }
+    // 20
+    public static void Singapore(double price) {
+        System.out.println("Price: $" + price);
+    }
+
+    public static void main(String[] args) {
+        bali("Bali");
+        bali(1200.50);
+        Paris("Paris");
+        Paris(1500.75);
+        Tokyo("Tokyo");
+        Tokyo(1800.30);
+        NewYork("New York");
+        NewYork(2200.99);
+        Dubai("Dubai");
+        Dubai(1300.40);
+        London("London");
+        London(1750.60);
+        Sydney("Sydney");
+        Sydney(2000.80);
+        Rome("Rome");
+        Rome(1450.90);
+        Bangkok("Bangkok");
+        Bangkok(950.25);
+        Singapore("Singapore");
+        Singapore(1700.45);
+    }
+}
