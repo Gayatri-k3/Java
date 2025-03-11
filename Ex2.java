526430650a3fd12592e008ea6b7d037171cd410b
Gayatri-k3 <gayatrik1029@gmail.com>
2025-03-11 08:05:04 +0530

    Update Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,114 @@
+public class Ex2 {
+    // 1
+    public static void kabaddi(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 2
+    public static void kabaddi(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 3
+    public static void Cricket(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 4
+    public static void Cricket(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 5
+    public static void Football(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 6
+    public static void Football(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 7
+    public static void Basketball(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 8
+    public static void Basketball(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 9
+    public static void Volleyball(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 10
+    public static void Volleyball(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 11
+    public static void Baseball(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 12
+    public static void Baseball(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 13
+    public static void Hockey(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 14
+    public static void Hockey(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 15
+    public static void Tennis(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 16
+    public static void Tennis(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 17
+    public static void Rugby(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 18
+    public static void Rugby(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    // 19
+    public static void Badminton(String name) {
+        System.out.println("Sport: " + name);
+    }
+    // 20
+    public static void Badminton(int players) {
+        System.out.println("Number of Players: " + players);
+    }
+
+    public static void main(String[] args) {
+        kabaddi("Kabaddi");
+        kabaddi(7);
+        Cricket("Cricket");
+        Cricket(11);
+        Football("Football");
+        Football(11);
+        Basketball("Basketball");
+        Basketball(5);
+        Volleyball("Volleyball");
+        Volleyball(6);
+        Baseball("Baseball");
+        Baseball(9);
+        Hockey("Hockey");
+        Hockey(11);
+        Tennis("Tennis");
+        Tennis(2);
+        Rugby("Rugby");
+        Rugby(15);
+        Badminton("Badminton");
+        Badminton(2);
+    }
+}
